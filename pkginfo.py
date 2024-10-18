95e2f05092218826e161fe50289d6a01b67e60dd
datluu <tad.louis2810@gmail.com>
2024-10-18 15:01:18 +1100

    all, sizes, search, version handle

--- pkginfo.py
+++ pkginfo.py
@@ -3,6 +3,10 @@ from __future__ import annotations
 import argparse
 import dataclasses
 
+import os
+import sys
+from datetime import datetime
+
 @dataclasses.dataclass
 class PkgInfo:
     category: str
@@ -14,6 +18,9 @@ class PkgInfo:
     def from_line(cls, line: str) -> PkgInfo:
         category, name, description, size = line.split(',')
         return PkgInfo(category, name, description, int(size))
+
+    def __str__(self) -> str:
+        return f'Package: {self.name}\nCategory: {self.category}\nDescription: {self.description}\nSize in kilobytes: {self.size}'
 
 
 @dataclasses.dataclass
@@ -29,16 +36,44 @@ class PkgInfoManager:
                 for line in f: pkgs.append(PkgInfo.from_line(line))
         # Handle exceptions
         except FileNotFoundError:
-            raise Exception(f"File {file_path} not found")
+            sys.exit(f"File {file_path} not found")
         except IOError:
-            raise Exception(f"Error reading file {file_path}")
+            sys.exit(f"Error reading file {file_path}")
         return PkgInfoManager(pkgs)
 
     # Get all packages names and print them
     def get_all(self) -> list[str]:
         res = [pkg.name for pkg in self.pkgs]
-        for name in res: print(name)
+        if res:
+            for name in res: print(name)
+        else:
+            print("No packages installed")
         return res
+
+    # Get all packages sizes and print them
+    def get_sizes(self) -> int:
+        sizes = sum([pkg.size for pkg in self.pkgs])
+        print(f'Total size in kilobytes: {sizes}')
+        return sizes
+    
+    # Get the version of the program
+    def get_version(self)  -> str:
+        student_name = 'Le Tuan Dat'
+        surname = 'Luu'
+        student_id = '24904308'
+        date = os.path.getmtime(__file__)
+        date = datetime.fromtimestamp(date).strftime('%d %b %Y')
+        ouput = f'Student Name: {student_name}\t Surname: {surname}\t Student ID: {student_id}\t Completion Date: {date}'
+        print(ouput)
+        return ouput
+
+    # Search for a package in the file
+    def search(self, name: str) -> PkgInfo:
+        for pkg in self.pkgs:
+            if pkg.name == name:
+                print(pkg)
+                return pkg
+        print(f'No installed package with this name: {name}')
 
 def main():
     # Create the argument parser
@@ -56,13 +91,17 @@ def main():
     args = parser.parse_args()
 
     if args.a:
-        print('a')
+        manager = PkgInfoManager.from_file(args.a)
+        manager.get_all()
     elif args.s:
-        print('s')
+        manager = PkgInfoManager.from_file(args.s)
+        manager.get_sizes()
     elif args.v:
-        print('v')
+        manager = PkgInfoManager.from_file(args.v)
+        manager.get_version()
     elif args.l:
-        print('l')
+        manager = PkgInfoManager.from_file(args.l[1])
+        manager.search(args.l[0])
 
 if __name__ == '__main__':
     main()
